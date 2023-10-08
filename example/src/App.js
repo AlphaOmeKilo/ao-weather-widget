307af2394592f2a9d62345b9db09f62d241cbe57
Alex OKeeffe <alexwokeeffe@gmail.com>
2023-10-08 15:57:28 +0100

    flesh out some more styles, add refresh functionality.

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,13 @@ import WeatherWidget from 'ao-weather-widget'
 import 'ao-weather-widget/dist/index.css'
 
 const App = () => {
-  return <WeatherWidget city="London" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+  return <div className='flex'>
+    <WeatherWidget city="London" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+    <WeatherWidget city="sevilla" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+    <WeatherWidget city="Paris" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+    <WeatherWidget city="Vancouver" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+    <WeatherWidget city="Bristol" apiToken="9a08c0bf4e4d59790f3633162aaf8f92"/>
+  </div>
 }
 
 export default App
--- src/WeatherWidget.js
+++ src/WeatherWidget.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import styles from './styles.module.scss'
+import styles from './assets/styles/styles.module.scss'
+import refreshIcon from 'assets/images/refresh.png'
 
 export const WeatherWidget = (props) => {
   const [data, setData] = useState(null)
   const [city, setCity] = useState(props.city)
   const [apiToken] = useState(props.apiToken)
+  const [splitCode, setSplitCode] = useState(0)
 
+  /**
+   * Call the openWeather API to get the current weather for the given
+   * city using the apiToken provided.
+   */
   const getWeather = () => {
     axios
       .get(
@@ -17,27 +23,51 @@ export const WeatherWidget = (props) => {
       })
   }
 
+  /**
+   * Get the first digit of a number
+   * @param {int} n
+   * @returns int
+   */
+  const getFirstDigit = (n) => {
+    // Find total number of digits - 1
+    const digits = Math.floor(Math.log(n) / Math.log(10))
+    // Find first digit
+    n = Math.floor(n / Math.pow(10, digits))
+    // Return first digit
+    return n
+  }
+
+  useEffect(() => {
+    setSplitCode(
+      `code-${
+        data
+          ? data.weather[0].id === 800
+            ? data.weather[0].id
+            : getFirstDigit(data.weather[0].id)
+          : 0
+      }`
+    )
+  }, [data])
+
   useEffect(() => {
     getWeather()
   }, [])
 
   return (
-    <div className={styles.widget}>
+    <div className={`${styles.widget} ${styles[splitCode]}`}>
       <div
         className={`${styles['widget-controls']} ${styles.flex} ${styles['flex-right']}`}
       >
-        {/* <img
-            className='refresh'
-            src={refreshIcon}
-            alt='refresh'
-            onClick={this.getWeather}
-          /> */}
+        <img
+          className={`${styles.refresh}`}
+          src={refreshIcon}
+          alt='refresh'
+          onClick={getWeather}
+        />
       </div>
 
       {data ? (
-        <div
-          className={`${styles['mt-8']} ${styles['black--text']} ${styles['text-align-center']}`}
-        >
+        <div className={`${styles['mt-8']} ${styles['text-align-center']}`}>
           <p className={`${styles['widget-city']}`}>{city}</p>
           <p className={`${styles['mt-1']}`}>{data.weather[0].description}</p>
           <img
